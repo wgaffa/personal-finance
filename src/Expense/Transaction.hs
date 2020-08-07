f1d7a1e9e9a20d749b96e9f653475d4dbfef7ede
Patrik Maunus <subscription@skriptladan.se>
2020-08-07 06:59:49 +0200

    Add Empty transaction
    An empty transaktion is the Nothing or No transaction of the type.

--- src/Expense/Transaction.hs
+++ src/Expense/Transaction.hs
@@ -5,25 +5,26 @@ module Expense.Transaction(
     , transaction
 ) where
 
-data Transaction a = Debit a | Credit a
+data Transaction a = Empty | Debit a | Credit a
     deriving (Show, Eq)
 
 instance (Ord a, Num a) => Semigroup (Transaction a) where
     (<>) = mappend
 
 instance (Ord a, Num a) => Monoid (Transaction a) where
-    mempty = Debit 0
+    mempty = Empty
     mappend a b = transaction (toNum a + toNum b)
 
 toNum :: (Num a) => Transaction a -> a
 toNum (Credit x) = negate x
 toNum (Debit x) = x
+toNum Empty = 0
 
 transaction :: (Ord a, Num a) => a -> Transaction a
 transaction amount
     | amount < 0 = credit amount
     | amount > 0 = debit amount
-    | otherwise = Debit 0
+    | otherwise = Empty
 
 credit :: (Num a) => a -> Transaction a
 credit = Credit . abs
