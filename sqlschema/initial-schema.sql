af7a14f846edafe81efb7aefd9d3d81ed696b1cb
Patrik Maunus <subscription@skriptladan.se>
2020-08-08 16:53:14 +0200

    Add sql schema scripts

---
+++ sqlschema/initial-schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "AccountElement" (
+	"id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	PRIMARY KEY("id")
+);
+
+CREATE TABLE "Accounts" (
+	"id"	INTEGER UNIQUE,
+	"name"	TEXT NOT NULL,
+	"element_id"	INTEGER NOT NULL,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("element_id") REFERENCES AccountElement ("id")
+);
+
+INSERT INTO "AccountElement" (name) VALUES ("Asset"), ("Liability"), ("Equity"), ("Income"), ("Expenses");
