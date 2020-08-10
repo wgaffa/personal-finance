1856961f0179c0e7274242207521c23c44d2f653
Patrik Maunus <subscription@skriptladan.se>
2020-08-10 18:59:38 +0200

    refactor: Reorganized file
    Still a mess but some api are in place

--- src/Expense/Transaction.hs
+++ src/Expense/Transaction.hs
@@ -6,6 +6,8 @@ module Expense.Transaction(
     , BalanceAmount(..)
     , BalanceSheet
     , Ledger(..)
+    , Transaction(..)
+    , TransactionEntry(..)
     , TransactionType(..)
     , credit
     , debit
@@ -14,6 +16,7 @@ module Expense.Transaction(
     , printableString
     , toNumeral
     , toSigNum
+    , transactionEntry
     , zeroBalance
 ) where
 
@@ -27,55 +30,13 @@ import qualified Data.Sequence as Seq
 -- time and date
 import Data.Time (Day)
 
-type BalanceSheet a = AccountElement -> [Ledger a] -> BalanceAmount a
-type ZeroBalance a = [TransactionAmount a] -> Bool
-
-data TransactionType = Debit | Credit
-    deriving (Eq, Enum, Bounded, Show)
-data BalanceAmount a = BalanceAmount TransactionType a
-    deriving (Show)
-
-data AccountElement = Asset | Liability | Equity | Income | Expenses
-    deriving (Ord, Eq, Enum, Bounded, Show)
-
-newtype PrintableString = PrintableString { unPrintableString :: Text.Text }
-    deriving (Ord, Eq, Show)
-
-data Account a = Account {
-    accountName :: PrintableString
-    , accountElement :: AccountElement
-} deriving (Ord, Eq, Show)
-
-data Ledger a = Ledger (Account a) [AccountTransaction a]
-    deriving(Show)
-
-data TransactionEntry a = TransactionEntry (Account a) (TransactionAmount a)
-
-data Transaction a = Transaction {
-    transactionDate :: Day
-    , transactionEntries :: [TransactionEntry a]
-}
-
-data AccountTransaction a = AccountTransaction {
-    atDate :: Day
-    , atAmount :: TransactionAmount a
-} deriving (Show)
-
-class AccountTypeable f where
-    transactionType :: f -> TransactionType
-    toBalance :: (Num a) => f -> a -> BalanceAmount a
-    toBalance l x = BalanceAmount (transactionType l) $ toSigNum (transactionType l) * x
-
 class Accountable f where
     increase :: (Num a) => f -> (a -> TransactionAmount a)
     decrease :: (Num a) => f -> (a -> TransactionAmount a)
 
-instance AccountTypeable AccountElement where
-    transactionType Asset     = Debit
-    transactionType Liability = Credit
-    transactionType Equity    = Credit
-    transactionType Income    = Credit
-    transactionType Expenses  = Debit
+instance Accountable (Account a) where
+    increase = increase . accountElement
+    decrease = increase . accountElement
 
 instance Accountable AccountElement where
     increase Asset     = debit
@@ -88,6 +49,28 @@ instance Accountable AccountElement where
     decrease Equity    = debit
     decrease Income    = debit
     decrease Expenses  = credit
+
+data TransactionType = Debit | Credit
+    deriving (Eq, Enum, Bounded, Show)
+
+data AccountElement = Asset | Liability | Equity | Income | Expenses
+    deriving (Ord, Eq, Enum, Bounded, Show)
+
+newtype PrintableString = PrintableString { unPrintableString :: Text.Text }
+    deriving (Ord, Eq, Show)
+
+data Account a = Account {
+    accountName :: PrintableString
+    , accountElement :: AccountElement
+} deriving (Ord, Eq, Show)
+
+data TransactionEntry a = TransactionEntry (Account a) (TransactionAmount a)
+    deriving (Show)
+
+data Transaction a = Transaction {
+    transactionDate :: Day
+    , transactionEntries :: [TransactionEntry a]
+} deriving (Show)
 
 data TransactionAmount a = TransactionAmount TransactionType a
     deriving (Show, Eq)
@@ -103,8 +86,38 @@ credit = TransactionAmount Credit . abs
 debit :: (Num a) => a -> TransactionAmount a
 debit = TransactionAmount Debit . abs
 
-zeroBalance :: (Eq a, Num a) => [AccountTransaction a]-> Bool
-zeroBalance xs = foldr ((+) . toNumeral . atAmount) 0 xs == 0
+transactionEntry :: (Num a) => (Account a -> a -> TransactionAmount a) -> Account a -> a -> TransactionEntry a
+transactionEntry f account = TransactionEntry account . f account
+
+-- experimental below
+type BalanceSheet a = AccountElement -> [Ledger a] -> BalanceAmount a
+type ZeroBalance a = [TransactionAmount a] -> Bool
+
+data BalanceAmount a = BalanceAmount TransactionType a
+    deriving (Show)
+
+data Ledger a = Ledger (Account a) [AccountTransaction a]
+    deriving(Show)
+
+data AccountTransaction a = AccountTransaction {
+    atDate :: Day
+    , atAmount :: TransactionAmount a
+} deriving (Show)
+
+class AccountTypeable f where
+    transactionType :: f -> TransactionType
+    toBalance :: (Num a) => f -> a -> BalanceAmount a
+    toBalance l x = BalanceAmount (transactionType l) $ toSigNum (transactionType l) * x
+
+instance AccountTypeable AccountElement where
+    transactionType Asset     = Debit
+    transactionType Liability = Credit
+    transactionType Equity    = Credit
+    transactionType Income    = Credit
+    transactionType Expenses  = Debit
+
+zeroBalance :: (Eq a, Num a) => [TransactionAmount a]-> Bool
+zeroBalance xs = foldr ((+) . toNumeral) 0 xs == 0
 
 toNumeral :: (Num a) => TransactionAmount a -> a
 toNumeral (TransactionAmount t x) = toSigNum t * x
