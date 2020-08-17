e3ff523c56d27db78c2c39c5988d7f0b3b65c313
Patrik Maunus <subscription@skriptladan.se>
2020-08-17 14:48:29 +0200

    feature: AbsoluteValue is a monoid

--- src/Expense/Transaction.hs
+++ src/Expense/Transaction.hs
@@ -79,6 +79,12 @@ data Account = Account {
 newtype AbsoluteValue a = AbsoluteValue { unAbsoluteValue :: a }
     deriving (Show)
 
+instance (Num a) => Semigroup (AbsoluteValue a) where
+    (AbsoluteValue a) <> (AbsoluteValue b) = AbsoluteValue (a + b)
+
+instance (Num a) => Monoid (AbsoluteValue a) where
+    mempty = AbsoluteValue 0
+
 -- | Constructor for 'AbsoluteValue'
 -- This creates an absolute value buy applying 'Prelude.abs' to the argument
 absoluteValue :: (Num a) => a -> AbsoluteValue a
