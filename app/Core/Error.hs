620ada586f28ea13557b143f77046ba6d58d01a6
Patrik Maunus <subscription@skriptladan.se>
2020-09-03 21:54:37 +0200

    fix: Error messages

--- app/Core/Error.hs
+++ app/Core/Error.hs
@@ -7,4 +7,10 @@ data AccountError
     | InvalidNumber
     | InvalidElement
     | AccountNotSaved String
-    deriving (Show)
+    | MiscError String
+
+instance Show AccountError where
+    show InvalidName = "account name is not valid"
+    show InvalidNumber = "account number must be positive"
+    show InvalidElement = "could not find account element"
+    show (AccountNotSaved s) = "could not save account, reason: " ++ s
