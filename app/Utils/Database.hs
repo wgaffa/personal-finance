c243e4d5370ef7360ea84eaac8d8d025f51d9549
Patrik Maunus <subscription@skriptladan.se>
2020-11-20 14:21:20 +0100

    Move sqlite instance to correct module

--- app/Utils/Database.hs
+++ app/Utils/Database.hs
@@ -1,15 +1,38 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Utils.Database (
     withDatabase,
 ) where
+
+import Data.Data (Typeable)
 
 import Control.Monad.Catch (bracket)
 import Control.Monad.IO.Class (MonadIO (liftIO))
 import Control.Monad.Reader (MonadReader (ask))
 
-import Database.SQLite.Simple (Connection, close, open)
+import Database.SQLite.Simple (
+    Connection,
+    ResultError (ConversionFailed),
+    SQLData (SQLInteger),
+    close,
+    open,
+ )
+import Database.SQLite.Simple.FromField (
+    FromField (..),
+    fieldData,
+    returnError,
+ )
+import Database.SQLite.Simple.Ok (Ok (Ok))
 
 import Core.App
 import Core.Database (enableForeignKeys)
+import Utility.Absolute (AbsoluteValue, absoluteValue)
+
+instance (Num a, Typeable a) => FromField (AbsoluteValue a) where
+    fromField f =
+        case fieldData f of
+            (SQLInteger i) -> Ok . absoluteValue . fromIntegral $ i
+            _ -> returnError ConversionFailed f "need a int"
 
 withDatabase :: (Connection -> App a) -> App a
 withDatabase f =
--- app/Utils/Maybe.hs
+++ app/Utils/Maybe.hs
@@ -1,30 +1,7 @@
-{-# OPTIONS_GHC -Wno-orphans #-}
-
 module Utils.Maybe (
     maybeToEither,
 ) where
 
-import Data.Data
-
-import Database.SQLite.Simple (
-    ResultError (ConversionFailed),
-    SQLData (SQLInteger),
- )
-import Database.SQLite.Simple.FromField (
-    FromField (..),
-    fieldData,
-    returnError,
- )
-import Database.SQLite.Simple.Ok (Ok (Ok))
-
-import Utility.Absolute (AbsoluteValue, absoluteValue)
-
 maybeToEither :: e -> Maybe a -> Either e a
 maybeToEither l Nothing = Left l
 maybeToEither _ (Just x) = Right x
-
-instance (Num a, Typeable a) => FromField (AbsoluteValue a) where
-    fromField f =
-        case fieldData f of
-            (SQLInteger i) -> Ok . absoluteValue . fromIntegral $ i
-            _ -> returnError ConversionFailed f "need a int"
